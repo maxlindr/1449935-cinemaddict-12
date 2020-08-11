8a216b69e4179ffe5c4e8fd4e0b4ce14fc160308
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-08-11 16:43:23 +0300

    Профиль пользователя теперь принимает реальные данные

--- src/view/profile.js
+++ src/view/profile.js
@@ -1,7 +1,20 @@
+const calcRank = (rating) => {
+  switch (true) {
+    case rating === 0:
+      return ``;
+    case rating <= 10:
+      return `novice`;
+    case rating <= 20:
+      return `fan`;
+    default:
+      return `movie buff`;
+  }
+};
+
 export const createProfileTemplate = (rating, avatarUrl) => {
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">${rating}</p>
+      <p class="profile__rating">${calcRank(rating)}</p>
       <img class="profile__avatar" src="${avatarUrl}" alt="Avatar" width="35" height="35">
     </section>`
   );
