fe6df265bb8cb052028623c226d945e6b2241cc9
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-08-16 23:37:55 +0300

    Фильтры как функции

--- src/mock/filters-mock.js
+++ src/mock/filters-mock.js
@@ -1,8 +1,8 @@
 export const createFilters = (movies) => {
   return {
-    all: movies.length,
-    watchlist: movies.filter((movie) => movie.watchlist).length,
-    history: movies.filter((movie) => movie.watched).length,
-    favorites: movies.filter((movie) => movie.favorite).length,
+    all: () => movies,
+    watchlist: () => movies.filter((movie) => movie.watchlist),
+    history: () => movies.filter((movie) => movie.watched),
+    favorites: () => movies.filter((movie) => movie.favorite),
   };
 };
--- src/view/main-menu.js
+++ src/view/main-menu.js
@@ -3,9 +3,9 @@ export const createMainMenuTemplate = (filters) => {
     `<nav class="main-navigation">
       <div class="main-navigation__items">
         <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filters.watchlist}</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${filters.history}</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filters.favorites}</span></a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filters.watchlist().length}</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${filters.history().length}</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filters.favorites().length}</span></a>
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
