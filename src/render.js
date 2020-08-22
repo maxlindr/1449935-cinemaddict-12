44453fd2a31a1d5dab7bd1b0d0671577ca004008
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-08-22 17:35:40 +0300

    В render и renderTemplate добавлена возможность работать с компонентами

--- src/render.js
+++ src/render.js
@@ -1,13 +1,27 @@
+import AbstractView from './view/abstract-view';
+
 export const RenderPosition = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`,
 };
 
 export const renderTemplate = (container, template, place) => {
+  if (container instanceof AbstractView) {
+    container = container.getElement();
+  }
+
   container.insertAdjacentHTML(place, template);
 };
 
 export const render = (container, element, place) => {
+  if (container instanceof AbstractView) {
+    container = container.getElement();
+  }
+
+  if (element instanceof AbstractView) {
+    element = element.getElement();
+  }
+
   switch (place) {
     case RenderPosition.AFTERBEGIN:
       container.prepend(element);
